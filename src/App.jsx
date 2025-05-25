fd405526d19f6d7b4e1ac83f281893de2baf06aa
Alejandro Diaz <alejandrodsfd@gmail.com>
2025-05-24 21:32:14 -0500

    Actualizaciones menores

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import { MyRoutes } from './routers/routes'
 import { Link } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 function App() {
 
   return (
-    <>
-     <Link className='titulo' to="/"><h1 >🎬 Peliculas</h1></Link>
-     <MyRoutes/>
-    </>
+    <HelmetProvider>
+        <BrowserRouter basename='/peliculas-react/'>
+          <Link className='titulo' to="/"><h1 >🎬 Peliculas</h1></Link>
+          <MyRoutes/>
+        </BrowserRouter>
+    </HelmetProvider>
   )
 }
 
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,12 @@
 import { ContextCard } from "../components/ContextCard/ContextCard";
-
+import { Helmet } from "react-helmet-async";
 export function LandingPage() {
     return(
         <div>
+            <Helmet>
+                <title>Peliculas App</title>
+                <meta name="description" content="Una app para buscar peliculas" />
+            </Helmet>
             <ContextCard/>
         </div>
     )
